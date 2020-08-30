b745395e224737a67f437647939767779f6c80fd
aldofusterturpin <aldoft@hotmail.com>
2020-08-30 13:01:05 +0200

    Fix error in table length when going back and fetching new data. Show timeline section just when checked in input form.

--- src/app/timeline-page/single-timeline/single-timeline.component.ts
+++ src/app/timeline-page/single-timeline/single-timeline.component.ts
@@ -5,7 +5,7 @@ import { Component, ViewChild, AfterViewInit, Input, OnInit, SimpleChanges, Outp
 import { SelectionModel } from '@angular/cdk/collections';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
-import { MatSnackBar, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { TimelineData } from '../../shared/timelineData';
 import { ElementType } from 'src/app/shared/ElementType';
@@ -49,32 +49,6 @@ export class SingleTimelineComponent implements OnInit, AfterViewInit {
     }
   }
 
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.tableDataSource.filter = filterValue.trim().toLowerCase();
-  }
-
-  isAllSelected() {
-    const numSelected = this.selection.selected.length;
-    const numRows = this.tableDataSource.data.length;
-    return numSelected === numRows;
-  }
-
-  /** Selects all rows if they are not all selected; otherwise clear selection. */
-  masterToggle() {
-    this.isAllSelected() ?
-      this.selection.clear() :
-      this.tableDataSource.data.forEach(row => this.selection.select(row));
-  }
-
-  /** The label for the checkbox on the passed row */
-  checkboxLabel(row?: any): string {
-    if (!row) {
-      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
-    }
-    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
-  }
-
   constructor(private timelineService: TimelineService, private _snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
@@ -83,27 +57,37 @@ export class SingleTimelineComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-    if (this.tableDataSource) {
-      this.tableDataSource.paginator = this.tablePaginator;
-    }
-
-    let message = "🧐Quicktip: select some rows of the table below and press the button 'Apply checkboxes filter' to see the changes";
-    let action = 'Got it!';
-    this._snackBar.open(message, action, {
-      duration: 20000,
-      verticalPosition: 'top',
-    });
   }
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes['timelineData']) {
+      console.log("changes['timelineData']");
+
       let tableData = this.createTableData(this.timelineData.apiResponse['Results']);
       this.tableLength = tableData.length;
       this.tableDataSource = new MatTableDataSource(tableData);
 
+      console.log("Below this.tablePaginator");
       console.log(this.tablePaginator);
+
+      console.log("Below tableData");
       console.log(tableData);
+
+      console.log("Below this.tableLength");
       console.log(this.tableLength);
+
+      //////////// this was in ngAfterViewInit
+      if (this.tableDataSource) {
+        this.tableDataSource.paginator = this.tablePaginator;
+      }
+  
+      let message = "🧐Quicktip: select some rows of the table below and press the button 'Apply checkboxes filter' to see the changes";
+      let action = 'Got it!';
+      this._snackBar.open(message, action, {
+        duration: 20000,
+        verticalPosition: 'top',
+      });
+      ////////////////////////////////////////////////////
 
       /* uncomment line below if want to show all the timeline elements on init  */
       //this.selectedData = this.timelineData.apiResponse['Results'];
@@ -148,4 +132,31 @@ export class SingleTimelineComponent implements OnInit, AfterViewInit {
   public shortRepresentationOf(d: Date) {
     return d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds() + ':' + d.getMilliseconds();
   }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.tableDataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.tableDataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    this.isAllSelected() ?
+      this.selection.clear() :
+      this.tableDataSource.data.forEach(row => this.selection.select(row));
+  }
+
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?: any): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
+  }
+
 }
